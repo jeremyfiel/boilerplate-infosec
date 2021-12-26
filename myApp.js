bcf078b732eb18af6a9f9513a812fffb2e274009
Jeremy Fiel <jeremyrfiel@gmail.com>
2021-12-25 22:00:12 -0500

    Commit made from VS Code Azure Static Web Apps

--- myApp.js
+++ myApp.js
@@ -1,50 +1,6 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require('helmet')
 
 
 module.exports = app;
